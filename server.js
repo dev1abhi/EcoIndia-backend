7401d13bf4c5959d34158c6ddac18f7112877480
DibyashaktIMoharana <dibyacollege@gmail.com>
2024-09-21 10:51:12 +0530

    commit reset

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 
-const port = 3000; 
+const port = 5000; 
 const cors = require("cors");
 const bodyParser = require('body-parser');
 
