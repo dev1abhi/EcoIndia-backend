b188228d4f9a9bd7c6ac9887e28704e98ee3c5bb
DibyashaktIMoharana <dibyacollege@gmail.com>
2024-09-21 10:45:57 +0530

    feat:port update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 
-const port = 5000; 
+const port = 3000; 
 const cors = require("cors");
 const bodyParser = require('body-parser');
 
